85a202c27dfbf4471a1fa2fade61cebd5715d0a0
jamann246 <93441782+jamann246@users.noreply.github.com>
2024-04-15 22:47:43 -0400

    data wrangling (may still need to discretize certain continous variables??)

---
+++ wrangle-data.R
@@ -0,0 +1,15 @@
+data_raw <- read.csv('./data/raw_data.csv')
+
+data <- 
+  data_raw |> 
+  dplyr::mutate(
+    dplyr::across(
+      dplyr::where(is.character), 
+      ~factor(stringr::str_to_title(.x))
+    ), 
+    dplyr::across(
+      .cols = c(CAEC, CALC), 
+      .fns = ~factor(.x, level = c("No","Sometimes","Frequently","Always"))
+    ),
+    BMI = Weight/Height^2
+    )
